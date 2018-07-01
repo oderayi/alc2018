0b181c85154ce0ea577f944a7ccc3418234aabc4
Steven Oderayi <oderayi@gmail.com>
2018-07-01 17:09:17 +0100

    Rebuild

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@
 import App from "./app/app";
 
 window.addEventListener("load", () => {
-  let root = "http://localhost:8080";
+  let root = '';//"http://localhost:8080";
   let root_url = root;
   let backend_api_root = '';
   var app = new App({ root_url: root_url, api_root: backend_api_root });
